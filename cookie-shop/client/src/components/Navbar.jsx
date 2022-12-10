5e1010284f900580a38a28a03402e6c64abdbb2b
Wicho <54341712+Wichosu@users.noreply.github.com>
2022-12-09 20:38:42 -0600

    adding styles for responsive

--- cookie-shop/client/src/components/Navbar.jsx
+++ cookie-shop/client/src/components/Navbar.jsx
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div className='sticky top-0 z-10'>
+    <div className='sticky top-0 z-10 bg-neutral-50'>
       <div 
-        className='flex px-16 justify-between items-center border-b-neutral-300 border-b-2
-        py-2 bg-neutral-50'
+        className='flex w-5/6 mx-auto justify-between items-center border-b-2
+        py-2'
       >
+        <div className='text-2xl md:hidden'>
+          <i className='bi bi-list'></i> 
+        </div>
         <div className='flex items-center gap-4'>
           <img className='w-12' src='./img/Cookie.svg' alt='Cookie shop logo' />
           <p className='text-2xl'>Cookie Shop</p>
         </div>
-        <ul className='w-fit mx-auto'>
+        <ul className='w-fit mx-auto hidden md:block'>
+          <Link className='mx-8'>All Cookies</Link>
           <Link className='mx-8'>Butter</Link>
           <Link className='mx-8'>Chocolate Chips</Link>
           <Link className='mx-8'>About Us</Link>
