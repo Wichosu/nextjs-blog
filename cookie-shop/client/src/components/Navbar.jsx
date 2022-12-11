7bb3262d11050a33bc0b135245a9af47e0448e84
Wicho <54341712+Wichosu@users.noreply.github.com>
2022-12-10 22:31:06 -0600

    Responsive implemented

--- cookie-shop/client/src/components/Navbar.jsx
+++ cookie-shop/client/src/components/Navbar.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const links = [
+    { name: 'All Cookies', link: '/'},
+    { name: 'Butter', link: '/'},
+    { name: 'Chocolate Chips', link: '/'},
+    { name: 'About Us', link: '/'}
+  ]
+
+  const [navBar, setNavBar] = useState(false);
+
+  const toggleNavBar = () => {
+    setNavBar(!navBar);
+  }
+
   return (
     <div className='sticky top-0 z-10 bg-neutral-50'>
       <div 
         className='flex w-5/6 mx-auto justify-between items-center border-b-2
         py-2'
       >
-        <div className='text-2xl md:hidden'>
+        <div 
+          className='absolute top-full left-0 right-0  bg-neutral-50 transition-all
+          duration-500 -z-10'
+          style={{transform: `translateY(-${!navBar * 100}vh)`}}
+        >
+          <ul className='flex flex-col w-5/6 mx-auto py-2 gap-2'>
+            { links.map((link, index) => (
+              <Link 
+                key={index}
+                className={`transition-all ${navBar? 'duration-700' : 'duration-75'}`} 
+                style={{opacity: `${navBar * 1}`}}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className='text-2xl md:hidden' onClick={toggleNavBar}>
           <i className='bi bi-list'></i> 
         </div>
         <div className='flex items-center gap-4'>
@@ -16,10 +47,14 @@ const Navbar = () => {
           <p className='text-2xl'>Cookie Shop</p>
         </div>
         <ul className='w-fit mx-auto hidden md:block'>
-          <Link className='mx-8'>All Cookies</Link>
-          <Link className='mx-8'>Butter</Link>
-          <Link className='mx-8'>Chocolate Chips</Link>
-          <Link className='mx-8'>About Us</Link>
+          { links.map((link, index) => (
+            <Link 
+              key={index}
+              className='mx-8' 
+            >
+              {link.name}
+            </Link>
+          ))}
         </ul>
         <div className='relative'>
           <i className='bi bi-cart-fill'></i>
