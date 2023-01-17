7cb54ca09311b03ce9da954a817671c770efcb1e
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-01-17 14:02:30 -0600

    html structure done

---
+++ web/components/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <hr />
+      <h1>Let's work together on something</h1>
+      <ul>
+      {/*Use The Image component and get social media logos locally*/}
+        <li>Linkedin</li>
+        <li>Github</li>
+        <li>Gmail</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,29 +1,66 @@
 import Link from 'next/link';
 import groq from 'groq';
 import client from '../client';
+import Image from 'next/image';
+import profilePic from '../public/examen1.png'; //temp profile pic
+import imageUrlBuilder from '@sanity/image-url';
+import Footer from '../components/Footer';
+
+function urlFor(source) {
+  return imageUrlBuilder(client).image(source)
+}
 
 const Index = ({posts}) => {
   return (
     <div>
-      <h1>Welcome to a blog!</h1>
+      <h1>Thoughts on my projects</h1>
+      <h2>A personal sight on what I've developed</h2>
+      <div>
+        <Image
+          src={profilePic} 
+          alt="Picture of Luis Eduardo Calderón Miranda"
+        />
+        <h3>Luis Eduardo Calderón Miranda (aka Wicho)</h3>
+        <p>A web developer who tries to improve its skills by building tiny projects</p>
+      </div>
       {posts.length > 0 && posts.map(
-        ({ _id, title = '', slug = '', publishedAt = '' }) => 
+        ({ _id, title = '', author = '', categories = '', slug = '', mainImage = '', publishedAt = '' }) => 
           slug && (
-            <li key={_id}>
+            <div key={_id}>
+              <img 
+                src={urlFor(mainImage)} 
+                alt='Image of the project'
+              />
+              <div>
+                <p>{author}</p>
+                <p>{new Date(publishedAt).toDateString()}</p>
+              </div>
               <Link href='/post/[slug]' as={`/post/${slug.current}`}>
                 {title}
-              </Link>{' '}
-              ({new Date(publishedAt).toDateString()})
-            </li>
+              </Link>
+              {/*Missing description*/}
+              <ul>
+                {categories.map(category => <li key={category}>{category}</li>)}
+              </ul>
+            </div>
           )
       )}
+      <Footer />
     </div>
   )
 }
 
 export async function getStaticProps(){
   const posts = await client.fetch(
-    groq`*[_type == "post" && publishedAt < now()] | order(publishedAt desc)`
+    groq`*[_type == "post" && publishedAt < now()]{
+      _id,
+      title,
+      "author": author->name,
+      mainImage,
+      slug,
+      publishedAt,
+      "categories": categories[]->title
+    } | order(publishedAt desc)`
   )
   
   return {
