b50347a1ad27e6c2a25c0c1dd25571121017f899
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-02-05 15:38:01 -0600

    reestructure project files

--- studio/sanity.cli.js
+++
@@ -1,9 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'f6kuvck3',
-    dataset: 'production',
-    apiVersion: '2023-01-27'
-  }
-})
