a14cc0267a27f7481d4567d165d8c31f16ee8283
Wicho <54341712+Wichosu@users.noreply.github.com>
2023-02-05 19:39:08 -0600

    another try

--- pages/index.js
+++ pages/index.js
@@ -115,7 +115,7 @@ export async function getStaticProps({ locale }){
     "url": post->url
   } | order(post->publishedAt desc)`
 
-  let posts = ''
+  let posts = {} 
 
   if(locale === 'en'){
     posts = await client.fetch(standardQuery)
